6dbfaf230256be2465245dbc35ca98b69cd789f9
rofl0r <retnyg@gmx.net>
2011-03-19 22:24:49 +0100

    merged SX changes

--- openbor.h
+++ openbor.h
@@ -792,10 +792,10 @@ typedef struct
 typedef struct {
 	char set;  
 	/*set determines how loading screen would be.
-	- -1 = default black screen with loading and status bar.
 	- 0 = no loading screen.
-	- 1 = loading screen background and status bar.
-	- 2 = only backg. image
+	- 1 = background and status bar.
+	- 2 = background only.
+	- 3 = status bar only.
 	
 	*/
 	char tf; //determines used font number for "LOADING" text (last element in command, moved here because of alignment)
@@ -1480,8 +1480,6 @@ typedef struct
 	Script          endlevel_script;
 	s_spawn_script_cache_node* spawn_script_cache_head;
 	int pos;
-	float advancex;
-	float advancey;
 	u32 advancetime;
 	u32 quaketime;
 	int quake;
