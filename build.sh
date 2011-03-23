392a25b88bae0b663e1b939c560bc55fe87e0f3a
rofl0r <retnyg@gmx.net>
2011-03-23 14:18:20 +0100

    some darwin toolchains changes by SX

--- build.sh
+++ build.sh
@@ -89,11 +89,9 @@ function distribute {
     cp ../scripts/packer ../../../engine/releases/DARWIN/OpenBOR.app/Contents/MacOS/
     cp ../scripts/paxplode ../../../engine/releases/DARWIN/OpenBOR.app/Contents/MacOS/
     cd ../../../engine
-  else
-    if [ `echo $HOST_PLATFORM | grep -o "Darwin"` ]; then
-      echo "Darwin Platform Failed To Build!"
-      exit 1
-    fi
+  else  
+    echo "Darwin Platform Failed To Build!"
+    exit 1
   fi
   
   echo "All Platforms Created Successfully"
@@ -352,14 +350,16 @@ function darwin {
       cp ./resources/PkgInfo ./releases/DARWIN/OpenBOR.app/Contents
       cp ./resources/Info.plist ./releases/DARWIN/OpenBOR.app/Contents
       cp ./resources/OpenBOR.icns ./releases/DARWIN/OpenBOR.app/Contents/Resources
-      cp ${DWNDEV}/lib/libSDL-1.2.0.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
-      cp ${DWNDEV}/lib/libSDL_gfx.13.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
-      cp ${DWNDEV}/lib/libogg.0.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
-      cp ${DWNDEV}/lib/libvorbisfile.3.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
-      cp ${DWNDEV}/lib/libvorbis.0.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
-      cp ${DWNDEV}/lib/libpng14.14.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
-      cp ${DWNDEV}/lib/libz.1.2.5.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
-      ./darwin.sh
+      if [ "${DWNDEV}" != "/opt/mac" ]; then
+        cp ${DWNDEV}/lib/libSDL-1.2.0.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
+        cp ${DWNDEV}/lib/libSDL_gfx.13.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
+        cp ${DWNDEV}/lib/libogg.0.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
+        cp ${DWNDEV}/lib/libvorbisfile.3.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
+        cp ${DWNDEV}/lib/libvorbis.0.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
+        cp ${DWNDEV}/lib/libpng14.14.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
+        cp ${DWNDEV}/lib/libz.1.2.5.dylib ./releases/DARWIN/OpenBOR.app/Contents/Libraries
+        ./darwin.sh
+      fi
     fi
     make clean BUILD_DARWIN=1
   fi
