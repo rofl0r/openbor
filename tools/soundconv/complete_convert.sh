ac62705fc66431d613aeb24478d407c7d81336aa
rofl0r <retnyg@gmx.net>
2012-02-13 21:49:42 +0100

    make complete_convert script more userfriendly

--- tools/soundconv/complete_convert.sh
+++ tools/soundconv/complete_convert.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 DIR=$(dirname "$(readlink -f "$0")")
 TARGET=$1
+if [[ -z "$TARGET" ]] ; then
+	echo need data directory of a mod as first argument!
+	exit 1
+fi
+
 if [[ -e "$TARGET/music" ]] ; then
 	$DIR/convertsound.pl "$TARGET"
 	if [[ "$?" == "0" ]] ; then
